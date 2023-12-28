eefa56039e69f8dd1ef2c9c23082765b11a1f615
Sydel Palinlin <sydel.palinlin@gmail.com>
2023-12-28 21:47:23 +0800

    wip

--- tests/feature/test_users_endpoints.py
+++ tests/feature/test_users_endpoints.py
@@ -1,7 +1,6 @@
 from fastapi import status
 
 
-# Test case for creating a user
 def test_create_user(client):
     payload = {
         "username": "testuseranother",
@@ -12,35 +11,28 @@ def test_create_user(client):
     assert response.status_code == status.HTTP_201_CREATED
     data = response.json()
     assert data["data"]["username"] == "testuseranother"
-    assert "email" in data["data"]  # Verify that the email is also returned
+    assert "email" in data["data"]
 
 
-# Test case for retrieving a list of users
 def test_read_users(client):
     response = client.get("/api/users")
     assert response.status_code == status.HTTP_200_OK
     data = response.json()
-    assert isinstance(data["data"], list)  # Confirm that it's a list
-    assert all(
-        "username" in user for user in data["data"]
-    )  # Check each user has a username
+    assert isinstance(data["data"], list)
+    assert all("username" in user for user in data["data"])
 
 
-# Test case for retrieving a single user
 def test_read_user(client):
-    user_id = (
-        1  # This should be replaced with a dynamic user creation and fetching of its ID
-    )
+    user_id = 1
     response = client.get(f"/api/users/{user_id}")
     assert response.status_code == status.HTTP_200_OK
     data = response.json()
     assert data["data"]["id"] == user_id
-    assert "username" in data["data"]  # Verify that the username is in the response
+    assert "username" in data["data"]
 
 
-# Test case for updating a user's information
 def test_update_user(client):
-    user_id = 1  # Replace with dynamic user creation
+    user_id = 1
     payload = {
         "username": "updateduser",
         "email": "updateduser@example.com",
@@ -50,11 +42,10 @@ def test_update_user(client):
     assert response.status_code == status.HTTP_200_OK
     data = response.json()
     assert data["data"]["username"] == "updateduser"
-    assert "email" in data["data"]  # Check for email in the response
+    assert "email" in data["data"]
 
 
-# Test case for deleting a user
 def test_delete_user(client):
-    user_id = 1  # Replace with dynamic user creation
+    user_id = 1
     response = client.delete(f"/api/users/{user_id}")
     assert response.status_code == status.HTTP_200_OK
