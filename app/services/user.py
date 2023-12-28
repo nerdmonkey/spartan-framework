fa55bd064b86fdf058f1093df3671777f439c257
Sydel Palinlin <sydel.palinlin@gmail.com>
2023-12-28 21:16:54 +0800

    wip

--- app/services/user.py
+++ app/services/user.py
@@ -7,8 +7,7 @@ from sqlalchemy.orm import Session
 
 from app.models.user import User
 from app.requests.user import UserCreateRequest, UserUpdateRequest
-from app.responses.user import (UserCreateResponse, UserResponse,
-                                UserUpdateResponse)
+from app.responses.user import UserCreateResponse, UserResponse, UserUpdateResponse
 
 
 class UserService:
