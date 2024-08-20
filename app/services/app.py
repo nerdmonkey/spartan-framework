50f8a830504e3d47c035fdd5642d550d904aa6ad
Sydel Palinlin <sydel.palinlin@gmail.com>
2024-08-20 13:31:41 +0800

    Rename license

--- app/services/app.py
+++ app/services/app.py
@@ -29,17 +29,17 @@ class AppService:
 
     @staticmethod
     def _load_config():
-            """Load configuration from environment variables.
+        """Load configuration from environment variables.
 
-            This function retrieves the table name and region name from environment variables.
-            If the environment variables are not set, default values are used.
+        This function retrieves the table name and region name from environment variables.
+        If the environment variables are not set, default values are used.
 
-            Returns:
-                tuple: A tuple containing the table name and region name.
-            """
-            table_name = os.environ.get("GSM_TABLE", "GlobalStateTable")
-            region_name = os.environ.get("AWS_REGION", "us-east-1")
-            return table_name, region_name
+        Returns:
+            tuple: A tuple containing the table name and region name.
+        """
+        table_name = os.environ.get("GSM_TABLE", "GlobalStateTable")
+        region_name = os.environ.get("AWS_REGION", "us-east-1")
+        return table_name, region_name
 
     @staticmethod
     def _setup_dynamodb():
@@ -84,81 +84,81 @@ class AppService:
         return jsonpickle.decode(value)
 
     def _handle_dynamodb_error(self, error, action):
-            """
-            Handle DynamoDB errors.
+        """
+        Handle DynamoDB errors.
 
-            This method is responsible for handling errors that occur during DynamoDB operations.
-            It logs the error message and raises the error.
+        This method is responsible for handling errors that occur during DynamoDB operations.
+        It logs the error message and raises the error.
 
-            Args:
-                error (Exception): The error that occurred during the DynamoDB operation.
-                action (str): The action being performed when the error occurred.
+        Args:
+            error (Exception): The error that occurred during the DynamoDB operation.
+            action (str): The action being performed when the error occurred.
 
-            Raises:
-                Exception: The original error that occurred during the DynamoDB operation.
+        Raises:
+            Exception: The original error that occurred during the DynamoDB operation.
 
-            """
-            self.logger.error(f"Error {action} state: {error}")
-            raise error
+        """
+        self.logger.error(f"Error {action} state: {error}")
+        raise error
 
     def set_state(self, key, value):
-            """
-            Sets or updates a state in the DynamoDB table.
+        """
+        Sets or updates a state in the DynamoDB table.
 
-            Args:
-                key (str): The key of the state to be set or updated.
-                value (Any): The value of the state to be set or updated.
+        Args:
+            key (str): The key of the state to be set or updated.
+            value (Any): The value of the state to be set or updated.
 
-            Returns:
-                Any: The updated value of the state.
+        Returns:
+            Any: The updated value of the state.
 
-            Raises:
-                boto3.exceptions.Boto3Error: If there is an error while setting or updating the state.
-            """
-            try:
-                value_json = self._serialize(value)
-                response = self.table.update_item(
-                    Key={"Key": key},
-                    UpdateExpression="SET Attr_Data = :val",
-                    ExpressionAttributeValues={":val": value_json},
-                    ReturnValues="UPDATED_NEW",
-                )
-                return response["Attributes"]["Attr_Data"]
-            except boto3.exceptions.Boto3Error as e:
-                self._handle_dynamodb_error(e, "setting")
+        Raises:
+            boto3.exceptions.Boto3Error: If there is an error while setting or updating the state.
+        """
+        try:
+            value_json = self._serialize(value)
+            response = self.table.update_item(
+                Key={"Key": key},
+                UpdateExpression="SET Attr_Data = :val",
+                ExpressionAttributeValues={":val": value_json},
+                ReturnValues="UPDATED_NEW",
+            )
+            return response["Attributes"]["Attr_Data"]
+        except boto3.exceptions.Boto3Error as e:
+            self._handle_dynamodb_error(e, "setting")
 
     def get_state(self, key):
-            """
-            Retrieves a state from the DynamoDB table.
+        """
+        Retrieves a state from the DynamoDB table.
 
-            Args:
-                key (str): The key used to retrieve the state.
+        Args:
+            key (str): The key used to retrieve the state.
 
-            Returns:
-                dict or None: The deserialized state if found, None otherwise.
-            """
-            try:
-                response = self.table.get_item(Key={"Key": key})
-                item = response.get("Item", {})
-                return self._deserialize(item["Attr_Data"]) if "Attr_Data" in item else None
-            except boto3.exceptions.Boto3Error as e:
-                self._handle_dynamodb_error(e, "getting")
+        Returns:
+            dict or None: The deserialized state if found, None otherwise.
+        """
+        try:
+            response = self.table.get_item(Key={"Key": key})
+            item = response.get("Item", {})
+            return self._deserialize(item["Attr_Data"]) if "Attr_Data" in item else None
+        except boto3.exceptions.Boto3Error as e:
+            self._handle_dynamodb_error(e, "getting")
 
     def remove_state(self, key):
-            """
-            Removes a state from the DynamoDB table.
+        """
+        Removes a state from the DynamoDB table.
 
-            Args:
-                key (str): The key of the state to be removed.
+        Args:
+            key (str): The key of the state to be removed.
 
-            Returns:
-                str: The attribute data of the removed state.
+        Returns:
+            str: The attribute data of the removed state.
 
-            Raises:
-                boto3.exceptions.Boto3Error: If there is an error while removing the state.
-            """
-            try:
-                response = self.table.delete_item(Key={"Key": key})
-                return response.get("Attributes", {}).get("Attr_Data")
-            except boto3.exceptions.Boto3Error as e:
-                self._handle_dynamodb_error(e, "removing")
+        Raises:
+            boto3.exceptions.Boto3Error: If there is an error while removing the state.
+        """
+        try:
+            response = self.table.delete_item(Key={"Key": key})
+            return response.get("Attributes", {}).get("Attr_Data")
+        except boto3.exceptions.Boto3Error as e:
+            self._handle_dynamodb_error(e, "removing")
