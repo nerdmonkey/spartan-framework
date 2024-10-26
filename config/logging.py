2729d4df733f5065a8dad9d04a3616fce5aac7c7
Sydel Palinlin <sydel.palinlin@gmail.com>
2024-10-27 04:36:53 +0800

    Update config file

--- config/logging.py
+++ config/logging.py
@@ -4,9 +4,3 @@ settings = get_settings()
 
 APP_NAME = settings.APP_NAME.lower()
 APP_ENVIRONMENT = settings.APP_ENVIRONMENT.lower()
-
-
-if APP_ENVIRONMENT in ["dev", "uat"]:
-    LOG_LEVEL = "DEBUG"
-else:
-    LOG_LEVEL = "INFO"
