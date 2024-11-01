31f4c65fcc0895639c054940d7616bbb1c452c4f
Sydel Palinlin <sydel.palinlin@gmail.com>
2024-11-01 11:37:53 +0800

    Add tests for logging service

--- app/middlewares/logging.py
+++ app/middlewares/logging.py
@@ -1,24 +1,36 @@
 from app.services.logging import StandardLoggerService
 
-standard_logger = StandardLoggerService()
 
+def standard_logging_middleware(handler, logger=None):
+    logger = logger or StandardLoggerService()
 
-def standard_logging_middleware(handler):
-    def wrapper(event, context):
-        buffer = {
-            "name": context.function_name,
-            "version": context.function_version,
-            "arn": context.invoked_function_arn,
-            "memory_size": context.memory_limit_in_mb,
-            "aws_request_id": context.aws_request_id,
-        }
+    def wrapped_handler(event, context):
+        try:
+            input_data_size = len(str(event).encode("utf-8"))
+            lambda_function = {
+                "name": context.function_name,
+                "version": context.function_version,
+                "arn": context.invoked_function_arn,
+                "memory_size": context.memory_limit_in_mb,
+                "aws_request_id": context.aws_request_id,
+            }
+            logger.info(
+                "Input Data",
+                input_data=event,
+                lambda_function=lambda_function,
+                input_data_size=input_data_size,
+            )
 
-        standard_logger.info("Input Data", input_data=event, lambda_function=buffer)
+            response = handler(event, context)
 
-        response = handler(event, context)
+            output_data_size = len(str(response).encode("utf-8"))
+            logger.info(
+                "Output Data", output_data=response, output_data_size=output_data_size
+            )
+            return response
 
-        standard_logger.info("Output Data", output_data=response)
+        except Exception as e:
+            logger.error("Error in Lambda function", error=str(e))
+            raise
 
-        return response
-
-    return wrapper
+    return wrapped_handler
