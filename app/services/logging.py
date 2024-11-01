14263e1892140adc38bb912b90c96bdb6091b12c
Sydel Palinlin <sydel.palinlin@gmail.com>
2024-11-01 18:26:02 +0800

    Add server logging feature

--- app/services/logging.py
+++ app/services/logging.py
@@ -1,5 +1,6 @@
 import json
 from logging import FileHandler
+from logging.handlers import SocketHandler
 
 from aws_lambda_powertools.logging import Logger
 from aws_lambda_powertools.logging.formatter import LambdaPowertoolsFormatter
@@ -11,7 +12,10 @@ settings = get_settings()
 APP_NAME = settings.APP_NAME.lower()
 LOG_LEVEL = settings.LOG_LEVEL
 LOG_FILE = settings.LOG_FILE
+LOG_CHANNEL = settings.LOG_CHANNEL
 APP_ENVIRONMENT = settings.APP_ENVIRONMENT.lower()
+TCP_LOG_HOST = "localhost"
+TCP_LOG_PORT = 9999
 
 
 class StandardLogFormatter(LambdaPowertoolsFormatter):
@@ -24,17 +28,38 @@ class FileLogFormatter(LambdaPowertoolsFormatter):
         return super().format(record)
 
 
+class ParseSocketHandler(SocketHandler):
+    def makePickle(self, record):
+        log_entry = json.loads(self.format(record))
+        return (json.dumps(log_entry) + "\n").encode("utf-8")
+
+
 class StandardLoggerService:
     def __init__(self):
-        if APP_ENVIRONMENT in ["local"]:
-            self.logger = Logger(
-                service=APP_NAME,
-                level=LOG_LEVEL,
-                formatter=FileLogFormatter(),
-                logger_handler=FileHandler(LOG_FILE),
-                json_deserializer=json.loads,
-            )
-        else:
+        try:
+            if APP_ENVIRONMENT in ["local"] and LOG_CHANNEL == "file":
+                self.logger = Logger(
+                    service=APP_NAME,
+                    level=LOG_LEVEL,
+                    formatter=FileLogFormatter(),
+                    logger_handler=FileHandler(LOG_FILE),
+                    json_deserializer=json.loads,
+                )
+
+            elif APP_ENVIRONMENT in ["local"] and LOG_CHANNEL == "server":
+                tcp_handler = ParseSocketHandler(TCP_LOG_HOST, TCP_LOG_PORT)
+                self.logger = Logger(
+                    service=APP_NAME,
+                    level=LOG_LEVEL,
+                    formatter=StandardLogFormatter(),
+                    logger_handler=tcp_handler,
+                    json_deserializer=json.loads,
+                )
+            else:
+                self.logger = Logger(
+                    service=APP_NAME, level=LOG_LEVEL, formatter=StandardLogFormatter()
+                )
+        except Exception as e:
             self.logger = Logger(
                 service=APP_NAME, level=LOG_LEVEL, formatter=StandardLogFormatter()
             )
