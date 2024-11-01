c8b4dea4133c9073b8fcaefac870937d6a36ae2a
Sydel Palinlin <sydel.palinlin@gmail.com>
2024-11-01 12:29:37 +0800

    Update test file

--- tests/unit/test_logging_service.py
+++ tests/unit/test_logging_service.py
@@ -1,6 +1,9 @@
-import pytest
 from unittest.mock import Mock, patch
+
+import pytest
+
 from app.services.logging import StandardLoggerService
+
 
 @pytest.fixture
 def mock_logger():
@@ -8,9 +11,11 @@ def mock_logger():
         mock_instance = MockLogger.return_value
         yield mock_instance
 
+
 @pytest.fixture
 def logging(mock_logger):
     return StandardLoggerService()
+
 
 def test_logger_info_called(logging, mock_logger):
     message = "Test info message"
@@ -18,11 +23,13 @@ def test_logger_info_called(logging, mock_logger):
 
     mock_logger.info.assert_called_once_with(message, key="value")
 
+
 def test_logger_error_called(logging, mock_logger):
     message = "Test error message"
     logging.error(message, error="TestError")
 
     mock_logger.error.assert_called_once_with(message, error="TestError")
+
 
 def test_logger_debug_called(logging, mock_logger):
     message = "Test debug message"
@@ -30,11 +37,13 @@ def test_logger_debug_called(logging, mock_logger):
 
     mock_logger.debug.assert_called_once_with(message, debug_data="sample")
 
+
 def test_logger_critical_called(logging, mock_logger):
     message = "Test critical message"
     logging.critical(message, critical_data="urgent")
 
     mock_logger.critical.assert_called_once_with(message, critical_data="urgent")
+
 
 def test_logger_exception_called(logging, mock_logger):
     message = "Test exception message"
