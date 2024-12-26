0abb9ef9b8a51c9bd7cacfe694645248d7ba9a61
Sydel Palinlin <sydel.palinlin@gmail.com>
2024-12-26 10:09:15 +0800

    Update environment variables and enhance Spark job logging in extract.py

--- jobs/extract.py
+++ jobs/extract.py
@@ -1,15 +1,23 @@
 import os
-
+import sys
 from awsglue.context import GlueContext
 from awsglue.utils import getResolvedOptions
-from pyspark.context import SparkContext
+from awsglue.job import Job
+from pyspark.sql import SparkSession
 
-sc = SparkContext()
-glueContext = GlueContext(sc)
+spark = SparkSession.builder.getOrCreate()
+glueContext = GlueContext(spark)
 
 logger = glueContext.get_logger()
 
 app_environment = os.getenv("APP_ENVIRONMENT", "test")
 
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+logger.info(f"Starting job: {args['JOB_NAME']}")
 logger.info(f"Currently in {app_environment} environment")
-logger.info("Hello, from Spartan")
+logger.info("Hello, from Spartan Job!")
+
+job.commit()
