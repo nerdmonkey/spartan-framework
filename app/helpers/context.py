1371100491d075620ea60ac87de5d1846a4a55b7
Sydel Palinlin <sydel.palinlin@gmail.com>
2025-02-23 06:35:19 +0800

    Update context.py

--- app/helpers/context.py
+++ app/helpers/context.py
@@ -42,12 +42,13 @@ class MockLambdaEvent:
         event (dict): A dictionary representing the mock event data with predefined key-value pairs.
     """
 
-    def __init__(self):
-        self.event = {
+    def __init__(self, event=None):
+        default_event = {
             "key1": "value1",
             "key2": "value2",
             "key3": "value3",
         }
+        self.event = event if event is not None else default_event
 
     def to_dict(self):
         """
