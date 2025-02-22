817dbe78d802c701afd588ea49f4e89e10514eac
Sydel Palinlin <sydel.palinlin@gmail.com>
2025-02-23 06:33:11 +0800

    Update __init__.py

--- app/helpers/logger/__init__.py
+++ app/helpers/logger/__init__.py
@@ -2,7 +2,12 @@ from app.helpers.environment import env
 from app.helpers.logger.factory import LoggerFactory
 
 
-def get_logger(service_name: str):
+def get_logger(service_name: str = None):
+
+    if not service_name:
+        service_name = "spartan-framework"
+
     environment = env("APP_ENVIRONMENT").lower()
     log_level = env("LOG_LEVEL") or "INFO"
+
     return LoggerFactory.create_logger(service_name, environment, log_level)
