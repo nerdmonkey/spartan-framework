cbf2bb13bc22fe8efd933c708ca760420dfd6863
Sydel Palinlin <sydel.palinlin@gmail.com>
2025-03-15 02:25:37 +0800

    Refactor predict handler to use custom logger from app.helpers; streamline logging setup

--- handlers/predict.py
+++ handlers/predict.py
@@ -1,15 +1,11 @@
 # handlers/predict.py
 from typing import Dict, Any
-from aws_lambda_powertools import Logger
+from app.helpers.logger import get_logger
 
-logger = Logger()
+logger = get_logger("spartan-framework")
 
-def get_logger():
-    return logger
 
 def main(event: Dict[str, Any], context: Any) -> Dict[str, Any]:
-    logger = get_logger()
-
     # Log the event and context
     logger.info("Event", extra={"event": event, "context": context.__dict__})
 
