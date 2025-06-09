9bad576eb258e0c72ccc7bb62d73a6bc901b6a30
Sydel Palinlin <sydel.palinlin@gmail.com>
2025-06-09 11:44:04 +0800

    Refactor main handler to log response instead of printing

--- handlers/inference.py
+++ handlers/inference.py
@@ -28,6 +28,6 @@ if __name__ == "__main__":
     context = MockLambdaContext()
 
     try:
-        print(main(event, context))
+        logger.info("Handler Response", extra={"response": main(event, context)})
     except Exception as e:
         logger.exception("Unhandled exception in main", extra={"error": str(e)})
