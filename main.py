98e055d78c993b55b3680805250b82256b92b8c5
Sydel Palinlin <sydel.palinlin@gmail.com>
2025-11-23 08:13:25 +0800

    Refactor main.py to move import statements outside of the main function for improved readability and organization; enhance logging setup for better error handling and response tracking.

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+from handlers.inference import main as inference_main
+
 def main(event, context):
     """Entry point for Google Cloud Functions."""
-    from handlers.inference import main as inference_main
     return inference_main(event, context)
+
+
+if __name__ == "__main__":
+    from app.helpers.context import MockLambdaContext, MockLambdaEvent
+    from app.helpers.logger import get_logger
+
+    logger = get_logger("spartan.inference")
+
+    event = MockLambdaEvent()
+    context = MockLambdaContext()
+
+    try:
+        logger.info("Handler Response", extra={"response": inference_main(event, context)})
+    except Exception as e:
+        logger.exception("Unhandled exception in main", extra={"error": str(e)})
